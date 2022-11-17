67159ac0009f041db98cf2960ad0c3f83e6b37a5
WalletJeremy <wallet.jeremy@gmail.com>
2022-11-17 21:54:55 +0100

    static card done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Header from '../../test-front-end/src/components/header/index';
-import Search from '../../test-front-end/src/components/search/index';
-import './App.css';
+import Header from './components/header/index';
+import Search from './components/search/index';
+import Card from './components/card/index';
+import './styles/App.css';
+// I went for a css reset to remove browser constraints
+import './styles/_reset.css';
 
 
 const App = () => (
   <div className="app">
     <Header />
     <Search />
+    <Card />
   </div>
 );
 
