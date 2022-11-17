68b6c1211f08d224211fdfb32394077cc0fa905a
WalletJeremy <wallet.jeremy@gmail.com>
2022-11-17 16:40:42 +0100

    implementation_of_the_search_input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../test-front-end/src/components/header/index';
+import Search from '../../test-front-end/src/components/search/index';
 import './App.css';
 
 
 const App = () => (
   <div className="app">
     <Header />
+    <Search />
   </div>
 );
 
